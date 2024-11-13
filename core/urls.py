45d74c2818607c9ba2762c0ee5b6e064bc6673db
Hossein Ahmadi <Hopoah1221@gmail.com>
2024-11-13 23:47:19 +0330

    finish Upadate

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("update-task/<int:pk>",updatetask, name="update"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import TaskForm
 
 def index(request):
-    return HttpResponse("HelloWorld")
+    form = TaskForm
+    tasks = Task.objects.all()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+    
+    return render(request,"tasks.html", context={
+        "tasks":tasks,
+        "TaskForm":form,
+    })
+
+def updatetask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    return render(request,"update.html", context={
+        "TaskForm":form,
+    })
